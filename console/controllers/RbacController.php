bcb6c47de932d1bee52aeb0fe7d3e16998ad23c5
derek <derekcz1991@gmail.com>
2017-03-06 18:53:51 +0800

    add rbac

---
+++ console/controllers/RbacController.php
@@ -0,0 +1,18 @@
+<?php
+use yii\console\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 06/03/2017
+ * Time: 6:30 PM
+ */
+class RbacController extends Controller
+{
+    public function actionInit()
+    {
+        $auth = Yii::$app->authManager;
+
+        
+    }
+}
